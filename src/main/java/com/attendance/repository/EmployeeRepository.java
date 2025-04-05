7ecb4f0c20204f20406414cb3e2fe566aa7a676d
Stubborn007 <rahulprajapati101202@gmail.com>
2025-04-05 13:08:56 +0530

    Add src/main/java/com/attendance/repository/EmployeeRepository.java 1743838736311

---
+++ src/main/java/com/attendance/repository/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package com.attendance.repository;
+
+import com.attendance.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+    Optional<Employee> findByEmail(String email);
+    boolean existsByEmail(String email);
+}
